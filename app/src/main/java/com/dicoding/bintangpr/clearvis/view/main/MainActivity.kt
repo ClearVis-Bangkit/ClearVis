514ce3984f0d7641af10fbe4825bbba9cc1391cd
bintangpr <bintangpr175@gmail.com>
2022-05-12 21:37:06 +0700

    slice login

--- app/src/main/java/com/dicoding/bintangpr/clearvis/view/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/bintangpr/clearvis/view/main/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.dicoding.bintangpr.clearvis
+package com.dicoding.bintangpr.clearvis.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.bintangpr.clearvis.R
+import com.dicoding.bintangpr.clearvis.view.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
     }
 }
