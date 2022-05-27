a2ede3ff4790171414b4f6b69b79d785f512c6ef
bintangpr <bintangpr175@gmail.com>
2022-05-27 15:22:42 +0700

    profile view model

--- app/src/main/java/com/dicoding/bintangpr/clearvis/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/dicoding/bintangpr/clearvis/view/profile/ProfileFragment.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.bintangpr.clearvis.data.preference.UserPreference
 import com.dicoding.bintangpr.clearvis.databinding.FragmentProfileBinding
+import com.dicoding.bintangpr.clearvis.view.factory.ViewModelFactory
 import com.dicoding.bintangpr.clearvis.view.login.LoginActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -26,7 +30,7 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupViewModel()
         binding?.logoutBtn?.setOnClickListener {
             profileViewModel.logout()
             Intent(requireContext(), LoginActivity::class.java).also { intent ->
@@ -34,6 +38,12 @@ class ProfileFragment : Fragment() {
                 requireActivity().finish()
             }
         }
+
+    }
+    private fun setupViewModel(){
+        profileViewModel.getUser().observe(viewLifecycleOwner, { user ->
+            binding?.nameTv?.text = user.name
+        })
     }
 
     override fun onDestroyView() {
